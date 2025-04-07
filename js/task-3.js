66547c12a6cfa792cd9d710e2e622f4197a99f93
Svitlana Shumal <svetlankachajka@gmail.com>
2025-04-07 15:35:45 +0300

    added fales

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getSubstring(string, length) {}
+
+console.log(getSubstring("Hello world", 3));
+console.log(getSubstring("Hello world", 5));
+console.log(getSubstring("Hello world", 8));
+console.log(getSubstring("Hello world", 11));
+console.log(getSubstring("Hello world", 0));
