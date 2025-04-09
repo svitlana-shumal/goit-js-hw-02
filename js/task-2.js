aaf13ea73e5e05669bc927a02389b3a792fd0724
Svitlana Shumal <svetlankachajka@gmail.com>
2025-04-09 10:38:20 +0300

    fixed fales

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@ function formatMessage(message, maxLength) {
   if (message.length <= maxLength) {
     return message;
   } else {
-    return message.slice(message, maxLength) + "...";
+    return message.slice(0, maxLength) + "...";
   }
 }
 
